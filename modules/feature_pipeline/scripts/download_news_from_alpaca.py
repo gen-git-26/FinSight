40c610bfa1549e3dba0eaeee5fb29edd6238f0e8
gen-git-26 <eliya@engineer.com>
2025-04-23 12:54:10 +0000

    generate data

--- modules/feature_pipeline/scripts/download_news_from_alpaca.py
+++ modules/feature_pipeline/scripts/download_news_from_alpaca.py
@@ -1,6 +1,11 @@
 from datetime import datetime
-from src.alpaca_news_api import download_historical_news
+import sys
+import os
 
+# # Add the correct path to the Python path
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+from src.alpaca_news_api import download_historical_news
 
 download_historical_news(
     from_date=datetime(2025, 4, 20),
